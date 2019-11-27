439677e501d0ed1a7bbac2f27ce37e9381bede5a
Kevin <kbmp82@gmail.com>
2019-11-26 17:03:05 -0700

    Finished sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 if(module.hot){
     module.hot.accept();
@@ -10,3 +11,5 @@ if(module.hot){
 const mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+
+let stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,64 @@
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
+
+class StickyHeader {
+    constructor() {
+        this.header = document.querySelector('.header');
+        this.logo = document.querySelector('.header__logo');
+        this.browserHeight = window.innerHeight;
+        this.previousScrollY = window.scrollY;
+        this.pageSections = document.querySelectorAll('.page-section');
+        this.resizeThrottle = debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 400)
+        this.events(); 
+    }
+
+    events() {
+        window.addEventListener('scroll', throttle(() => this.runOnScroll(), 200));
+        window.addEventListener('resize', this.resizeThrottle);
+    }
+
+    runOnScroll() {
+        this.determineScrollDirection();
+
+        if (window.scrollY > 60) {
+          this.header.classList.add('header--dark'); 
+          this.logo.classList.add('header__logo--shrink'); 
+        }else{
+            this.header.classList.remove('header--dark'); 
+            this.logo.classList.remove('header__logo--shrink');
+        }
+
+        this.pageSections.forEach(el => this.checkSection(el))
+    }
+
+    determineScrollDirection() {
+        if(window.scrollY > this.previousScrollY){
+            this.scrollDirection = 'down';
+        }else{
+            this.scrollDirection = 'up'; 
+        }
+        this.previousScrollY = window.scrollY;
+
+        console.log(this.scrollDirection);
+    }
+
+    //check browser page section and highlight corresponding nav href
+    checkSection(el) {
+        console.log(window.scrollY)
+        if(window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < (el.offsetTop + el.offsetHeight)) {
+            let scrollPercent = el.getBoundingClientRect().y / this.browserHeight * 100
+            console.log(scrollPercent)
+            if(scrollPercent < 33 && scrollPercent > -0.1 && this.scrollDirection === 'down' || scrollPercent < 33 && this.scrollDirection === 'up' ){
+              let matchingLink = el.getAttribute('data-matching-link');
+              document.querySelectorAll(`.nav a:not(${matchingLink})`).forEach(link => link.classList.remove('nav--is-current'));
+              document.querySelector(matchingLink).classList.add('nav--is-current');
+            }else if(window.scrollY < 300){
+                document.querySelectorAll('.nav a').forEach(link => link.classList.remove('nav--is-current'));
+            }
+        }
+    }
+}
+
+export {StickyHeader as default}
