dbc030f6a4d9c9886b9e50674c20f130a601cf5c
Kevin <kbmp82@gmail.com>
2019-11-27 14:52:52 -0700

    Completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,12 +4,27 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
+new MobileMenu();
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new StickyHeader();
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => el.addEventListener('click', e => {
+    e.preventDefault();
+    if(typeof modal == 'undefined') {
+        //webpackChunkName set name for bundle that is created. Can view it network tab of dev console
+        import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+            modal = new x.default();
+            setTimeout(() => modal.openModal(), 20);
+         }).catch(() => console.log('error'));
+    }else{
+       modal.openModal();
+    }
+}))
+
+
 if(module.hot){
     module.hot.accept();
 }
 
-const mobileMenu = new MobileMenu();
-new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
-
-let stickyHeader = new StickyHeader();
