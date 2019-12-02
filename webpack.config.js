0181a914b08ef7a692e39a0d97cf20b431fa3979
Kevin <kbmp82@gmail.com>
2019-12-02 10:00:53 -0700

    Completed build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 //jshint esversion:6
-const path = require('path')
+const currentTask = process.env.npm_lifecycle_event;
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // removes old dynamically generated file when re-building
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //extracts CSS into its own file on build
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //inserts dynamically generated filenames into html
+const fse = require('fs-extra'); //for working with websites containing multiple html files
 
 const postCSSPlugins = [
 require('postcss-import'),
@@ -8,35 +13,98 @@ require('postcss-simple-vars'),
 require('postcss-nested'),
 require('postcss-hexrgba'),
 require('autoprefixer'),
-]
-module.exports = {
+];
+
+class RunAfterCompile {
+apply(compiler) {
+    compiler.hooks.done.tap('Copy images', () => {
+        fse.copySync('./app/assets/images', './docs/assets/images')
+    });
+}
+}
+
+//apply HtmlWebpackPlugin to each html file
+let pages = fse.readdirSync('./app').filter((file) => {
+    return file.endsWith('.html');
+}).map((page) => {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    });
+});
+
+let cssConfig =  {
+    test: /\.css$/i, 
+    use: [
+        'css-loader',{
+            loader: 'postcss-loader',
+            options: {
+                plugins: postCSSPlugins
+            }
+        }
+    ]
+};
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
+    plugins: pages,
+    module: {
+        rules: [
+           cssConfig
+        ]
+    }
+};
+
+if (currentTask === 'dev') {
+
+    cssConfig.use.unshift('style-loader');
+    config.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
-    },
-    mode: 'development',
-    // watch: true, --dev server automatically watches so this is not needed
-    devServer: {
+    };
+
+    config.devServer = {
         
         contentBase: path.join(__dirname, 'app'),
         hot: true,
-        host: '0.0.0.0', //use my IPv4 address...allows any PC (or phone) on the network to see the dev page
+        host: '0.0.0.0', //use my IPv4 address to view on web browser...allows any PC (or phone) on the network to see the dev page
         port: 3000
-      },
-      module: {
-          rules: [
-              {
-                  test: /\.css$/i, 
-                  use: [
-                      'style-loader','css-loader?url=false',{
-                          loader: 'postcss-loader',
-                          options: {
-                              plugins: postCSSPlugins
-                          }
-                      }
-                  ]
-              }
-          ]
-      }
+      };
+
+      config.mode = 'development';
+
+}else if(currentTask === 'build') {
+
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    });
+
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    };
+
+    config.mode = 'production';
+    config.optimization = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
+    config.plugins.push(
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+        );
+    postCSSPlugins.push(require('cssnano'));
 }
+
+module.exports = config;
