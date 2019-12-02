484c7e7ed56816eddbb70b5baf3875384f09e6ac
Kevin <kbmp82@gmail.com>
2019-12-02 15:19:28 -0700

    Updated cloud funciton for secret password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'welcome to the secret area'
-    })
+    let body;
+
+    const secretContent = `
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong> and 2+2 = 4</p>
+    `;
+
+    if(event.body) {
+      body = JSON.parse(event.body);  
+    }else {
+        body = {};
+    }
+
+    if(body.password === 'javascript') {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    }else {
+        callback(null, {
+            statusCode: 401,
+            body: 'You do not have access to this content'
+        })
+    }
+
+   
 }
